d60295f3fb907f554da070e008e53a3fd262e20a
Yolandaokyere <yolandaokyere@outlook.com>
2022-03-18 11:53:23 +0100

    css last changes

--- OBA.project/script.js
+++ OBA.project/script.js
@@ -1,7 +1,7 @@
 import { fetchData } from "./fetchData.js";
 
  fetchData({query: 'vegan', destination: '.media-group1 ul'}) // Fetch Vegan data 
- fetchData({query: 'protein', destination: '.media-group2 ul'}) // Fetch Protein data 
+ fetchData({query: 'eiwitten', destination: '.media-group2 ul'}) // Fetch Protein data 
  fetchData({query: 'intermittent fasting', destination: '.media-group3 ul'}) // Fetch Fasting data 
 
 // Hashtags linkjes
